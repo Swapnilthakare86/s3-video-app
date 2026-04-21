860086b324a82361d6922c889ed63a3370ca7a79
Swapnilthakare86 <sthakare@xtsworld.in>
2026-04-21 18:43:34 +0530

    modify footere

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p>MovieFlex</p>
+      <p>MovieFlex- Secure private streaming from S3</p>
     </footer>
   );
 }
